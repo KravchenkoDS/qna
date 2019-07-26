2ea55dcdb38564848af608006b0a8ea8e5919c08
kds <ds.krav@gmail.com>
2019-07-26 09:04:28 +0800

    add Active Storage

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,8 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :user
-  #belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+
+  has_many_attached :files
 
   validates :title, :body, presence: true
 end
