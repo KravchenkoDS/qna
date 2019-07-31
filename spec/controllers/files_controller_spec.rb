b894db6fc40423d025cf11950d489cfbe151e8cd
kds <ds.krav@gmail.com>
2019-07-31 16:29:50 +0800

    add awards and links

--- spec/controllers/files_controller_spec.rb
+++ spec/controllers/files_controller_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe FilesController, type: :controller do
 
   let(:user) { create(:user) }
+
   before { login(user) }
 
   describe 'DELETE #destroy' do
     let!(:file) { fixture_file_upload("#{Rails.root}/spec/rails_helper.rb") }
 
-    context 'author tried delete file' do
+    context 'author delete file' do
       let(:own_answer) { create(:answer, user: user, files: file) }
       it 'deletes the file from  answers files' do
         expect { delete :destroy, params: { id: own_answer.files.first}, format: :js }
@@ -21,7 +22,7 @@ RSpec.describe FilesController, type: :controller do
       end
     end
 
-    context ' Not author tried delete file' do
+    context ' Not author delete file' do
       let(:answer) { create(:answer, files: file) }
       it 'no deletes the file' do
         expect { delete :destroy, params: { id: answer.files.first }, format: :js }
