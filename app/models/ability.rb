0cb3ea4f6b3c1fba0b044d40dffe7158cecc8329
kds <ds.krav@gmail.com>
2019-08-22 18:59:18 +0800

    add gem cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,31 +4,40 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    include CanCan::Ability
+
+    def initialize(user)
+      if user
+        user.admin? ? admin_abilities : user_abilities(user)
+      else
+        guest_abilities
+      end
+    end
+
+    def guest_abilities
+      can :read, :all
+    end
+
+    def user_abilities(user)
+      guest_abilities
+      can :create, [Question, Answer, Comment]
+      can :update, [Question, Answer], user_id: user.id
+      can :destroy, [Question, Answer], user_id: user.id
+      can :best, Answer, question: { user_id: user.id }
+      can :manage, ActiveStorage::Attachment do |file|
+        user.author? file.record
+      end
+
+      can [:vote_up, :vote_down], [Question, Answer] do |item|
+        !user.author? item
+      end
+      can :vote_destroy, [Question, Answer], votes: { user_id: user.id }
+      can [:create, :destroy], Link, linkable: { user_id: user.id }
+      can :create, Award, question: { user_id: user.id  }
+    end
+
+    def admin_abilities
+      can :manage, :all
+    end
   end
 end
