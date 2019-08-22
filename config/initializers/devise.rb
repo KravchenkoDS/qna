25115af52748fe5112dbc09cbda525e5b6c9a729
kds <ds.krav@gmail.com>
2019-08-22 18:51:20 +0800

    add gem cancancan

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,7 +261,6 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-
   config.omniauth :github,
                   Rails.application.credentials[Rails.env.to_sym][:github][:app_id],
                   Rails.application.credentials[Rails.env.to_sym][:github][:app_secret],
