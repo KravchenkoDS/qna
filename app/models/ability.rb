c4064a405d9f81a6c6d01988ea080dcf1d1360e0
kds <ds.krav@gmail.com>
2019-08-26 22:48:09 +0800

    add gem cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,41 +3,39 @@
 class Ability
   include CanCan::Ability
 
+  attr_reader :user
+
   def initialize(user)
-    include CanCan::Ability
-
-    def initialize(user)
-      if user
-        user.admin? ? admin_abilities : user_abilities(user)
-      else
-        guest_abilities
-      end
-    end
-
-    def guest_abilities
-      can :read, :all
-    end
-
-    def user_abilities(user)
+    if user
+      user.admin? ? admin_abilities : user_abilities
+    else
       guest_abilities
-      can :create, [Question, Answer, Comment]
-      can :update, [Question, Answer], user_id: user.id
-      can :destroy, [Question, Answer], user_id: user.id
-      can :best, Answer, question: { user_id: user.id }
-      can :manage, ActiveStorage::Attachment do |file|
-        user.author? file.record
-      end
-
-      can [:vote_up, :vote_down], [Question, Answer] do |item|
-        !user.author? item
-      end
-      can :vote_destroy, [Question, Answer], votes: { user_id: user.id }
-      can [:create, :destroy], Link, linkable: { user_id: user.id }
-      can :create, Award, question: { user_id: user.id  }
     end
+  end
 
-    def admin_abilities
-      can :manage, :all
+  def guest_abilities
+    can :read, :all
+  end
+
+  def user_abilities
+    guest_abilities
+    #cannot?
+    can :create, [Question, Answer, Comment]
+    can :update, [Question, Answer], user_id: user.id
+    can :destroy, [Question, Answer], user_id: user.id
+    can :best, Answer, question: {user_id: user.id}
+    can :vote_destroy, [Question, Answer], votes: {user_id: user.id}
+    can [:create, :destroy], Link, linkable: {user_id: user.id}
+    can :create, Award, question: {user_id: user.id}
+    can [:vote_up, :vote_down], [Question, Answer] do |item|
+      !user.author? item
     end
+    can :manage, ActiveStorage::Attachment do |file|
+      user.author? file.record
+    end
+  end
+
+  def admin_abilities
+    can :manage, :all
   end
 end
