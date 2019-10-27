fd0fc44bf08971a65d2a82c583237e5b64ee38aa
kds <ds.krav@gmail.com>
2019-10-27 19:01:46 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,3 +40,4 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
+set :rbenv_map_bins, %w(rake gem bundle ruby rails sidekiq sidekiqctl)
