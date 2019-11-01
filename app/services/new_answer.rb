fe1767a831050317f54e256e431ee2a0b0135806
kds <ds.krav@gmail.com>
2019-11-01 13:20:26 +0800

    deploy

--- app/services/new_answer.rb
+++ app/services/new_answer.rb
@@ -1,6 +1,7 @@
 class Services::NewAnswer
   def send_notification(answer)
-    answer.question.subscriptions.includes(:user).find_each do |subscription|
+    #answer.question.subscriptions.includes(:user).find_each do |subscription|
+    answer.question.subscriptions.find_each do |subscription|
       NewAnswerMailer.notification(subscription.user, answer).deliver_later
     end
   end
