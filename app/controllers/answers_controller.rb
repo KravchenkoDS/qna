7f7c023dfedca57faf406db4ab3d27febfd537c0
kds <ds.krav@gmail.com>
2019-11-02 13:25:58 +0800

    deploy

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -49,8 +49,8 @@ class AnswersController < ApplicationController
   def publish_answer
     return if @answer.errors.any?
 
-    AnswersChannel.broadcast_to(answer.question, answer: answer,
-        links: answer.links, files: answer.files.map { |file| { id: file.id, name: file.filename.to_s, url: url_for(file) } }
+    AnswersChannel.broadcast_to(@answer.question, answer: @answer,
+        links: @answer.links, files: @answer.files.map { |file| { id: file.id, name: file.filename.to_s, url: url_for(file) } }
     )
   end
 
